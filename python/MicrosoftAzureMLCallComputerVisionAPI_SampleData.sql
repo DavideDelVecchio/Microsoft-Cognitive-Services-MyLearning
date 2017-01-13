527d4e25100b3438f87b6a68b223453d12b29443
Alexander Todorovic <alexandertodorovic@ieee.org>
2017-01-13 23:06:27 +0100

    Sample Data added

---
+++ python/MicrosoftAzureMLCallComputerVisionAPI_SampleData.sql
@@ -0,0 +1,24 @@
+CREATE TABLE [dbo].[ImageDescription](
+	[urlid] [tinyint] NOT NULL,
+	[url] [varchar](max) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (1, N'http://unsplash.com/photos/98Rcate6GCU/download?force=true')
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (2, N'http://unsplash.com/photos/w9CKc-BvSrg/download?force=true')
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (3, N'http://unsplash.com/photos/GL6ORxDMswI/download?force=true')
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (4, N'http://unsplash.com/photos/usk52YRwGrM/download?force=true')
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (5, N'http://unsplash.com/photos/LNOJuUSJZqM/download?force=true')
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (6, N'http://unsplash.com/photos/QY300DHjPaQ/download?force=true')
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (7, N'http://unsplash.com/photos/EfhCUc_fjrU/download?force=true')
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (8, N'http://unsplash.com/photos/C3pE8fJJ3bg/download?force=true')
+GO
+INSERT [dbo].[ImageDescription] ([urlid], [url]) VALUES (9, N'http://unsplash.com/photos/kRqA1yFZp1s/download?force=true')
+GO
